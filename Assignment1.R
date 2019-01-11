f392158e4aa7aaa9d255f547f6e278bbf62a95ec
audreyew <wilsoa38@mcmaster.ca>
2019-01-11 10:58:07 -0500

    assignment1

--- Assignment1.R
+++ Assignment1.R
@@ -1,6 +1,4 @@
 #bring in data set
-download.file(url = "https://raw.githubusercontent.com/audreyew/QMEE/master/FO_TempPlasticitySelection_complete_April2018.csv",
-              destfile = "data/FO_TempPlasticitySelection_complete_April2018.csv")
 library(readr)
 data <- read_csv("FO_TempPlasticitySelection_complete_April2018.csv")
 View(data)
