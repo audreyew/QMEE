a23968c561bd2f3ed188d9753fd8bf6171144b48
audreyew <wilsoa38@mcmaster.ca>
2019-01-25 00:20:00 -0500

    For assignment 3

--- Assignment3.R
+++ Assignment3.R
@@ -6,7 +6,7 @@ load(file = "data/QMEEdata.RData")
 #data needed some more cleaning and a name change
 FlyvsPreds <- (data_2 %>% mutate(temperature=as.factor(temperature), survival = as.factor(survival)))
 
-#plot1:This plot is trying to show the size differences of the flies that are reared at different temperatures
+#Plot1:This plot is trying to show the size differences of the flies that are reared at different temperatures
 #I took out data that had no trait or sex identified as these are errors in data entry and including them makes the graph messy and hard to read
 #Since my priority was to look at temperature and size, these are on the main axis 
 #sex is separated by colour because this is also known to affect fruit fly size (males are typically smaller)
@@ -21,7 +21,7 @@ FlyvsPreds <- (data_2 %>% mutate(temperature=as.factor(temperature), survival = 
   + scale_color_brewer(palette = "Dark2")
 )
 
-#plot2:How long did individuals survive between the predator and the non-predator treatments? 
+#Plot2:How long did individuals survive between the predator and the non-predator treatments? 
 #grouping the dataset first should avoid counting individuals more than once (some individuals have the same number id but are in different treatments)
 (FlyvsPreds
   %>% group_by(individual, cage, sex)
@@ -32,7 +32,7 @@ FlyvsPreds <- (data_2 %>% mutate(temperature=as.factor(temperature), survival = 
   )
 #wanted to put the proportions on the graph so it was easier to tell the values of the points but I couldn't figure out how with my set up
 
-#plot3:Do individuals that are larger survive for longer?
+#Plot3:Do individuals that are larger survive for longer?
 #filtered to remove data entry errors and grouped to avoid (or try to avoid) counting the same individual twice
 #included both plot points and lines to get a better idea of what is going on with the data
 (FlyvsPreds
